1382d3be26210476cf40ca9565277a875c03c47c
Laurence Russell <ljr1e21@soton.ac.uk>
2025-05-06 12:24:44 +0100

    Added test for generating matrix with coefficients

--- tests/test_RBF_morpher.py
+++ tests/test_RBF_morpher.py
@@ -26,7 +26,32 @@ def test_generate_interpolation_matrix():
     expected_matrix = np.zeros((3, 3))
     for i in range(3):
         for j in range(3):
-            expected_matrix[i][j] = custom_RBF(np.linalg.norm(original_vertices[i] - original_vertices[j]))
+            expected_matrix[i][j] = custom_RBF(
+                np.linalg.norm(original_vertices[i] - original_vertices[j])
+                )
 
     # Assert that the generated interpolation matrix matches the expected matrix
     np.testing.assert_array_almost_equal(morpher.interp_matrix, expected_matrix)
+
+def test_generate_coefficient_matrix():
+    # Mock data for original and displaced meshes
+    original_vertices = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]])
+    displaced_vertices = np.array([[0, 0, 0], [1, 1, 0], [0, 1, 1]])
+
+    original_mesh = MockMesh(original_vertices)
+    displaced_mesh = MockMesh(displaced_vertices)
+
+    # Instantiate RBFMorpher with the custom RBF function
+    morpher = RBFMorpher(original_mesh, displaced_mesh, custom_RBF)
+
+    # Manually compute the expected coefficient matrix
+    interp_matrix = np.zeros((3, 3))
+    for i in range(3):
+        for j in range(3):
+            interp_matrix[i][j] = custom_RBF(
+                np.linalg.norm(original_vertices[i] - original_vertices[j])
+                )
+    expected_coeff_matrix = np.linalg.inv(interp_matrix) @ (displaced_vertices - original_vertices)
+
+    # Assert that the generated coefficient matrix matches the expected matrix
+    np.testing.assert_array_almost_equal(morpher.coeff_matrix, expected_coeff_matrix)
